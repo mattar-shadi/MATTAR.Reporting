b851c2c5c07503b35cac82991c302186e838b915
Shadi MATTAR <shadi@mattar.fr>
2026-04-22 19:20:13 +0200

    Update IReport.cs

--- src/Interfaces/IReport.cs
+++ src/Interfaces/IReport.cs
@@ -1,16 +1,6 @@
-﻿namespace MATTAR.Reporting
+namespace MATTAR.Reporting
 {
     public interface IReport
-    {
-        string GenerateReport(
-            string templateDocPath,
-            string outputPathFile,
-            Dictionary<string, string?> datas,
-            string? ownerPassword = "MATTAR.Reporting",
-            Dictionary<string, string?>? images = null);
-    }
-
-    public interface ITableReport : IReport
     {
         string GenerateReport(
             string templateDocPath,
